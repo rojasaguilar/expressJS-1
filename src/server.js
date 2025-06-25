d0a198bfb68d6160b5cba11c964d641fce0e5dee
Ramses Rojas <rojas.aguilar.ramses721@outlook.com>
2025-06-25 16:13:40 -0700

    server running

--- src/server.js
+++ src/server.js
@@ -1,3 +1,21 @@
 const express = require('express')
+const nodemon = require('nodemon')
 
-const
+const app = express();
+
+app.use(express.json());
+
+app.get('/' , (req,res) => {
+    res.json('hello')
+})
+
+app.get('/home' , (req,res) => {
+    console.log(req)
+    res.json('hello')
+})
+
+app.listen(8899,() => {
+    console.log("listening in the port 8899..")
+})
+
+
